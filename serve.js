087386433bef7a66ce42cee0c8d1a1912a02c969
timglorioso <glorioso.tim@gmail.com>
2016-01-03 20:41:19 -0500

    serve PDF content (development server)

--- serve.js
+++ serve.js
@@ -4,6 +4,7 @@ var httpd = require('node-http-server');
 
 var config = httpd.configTemplate();
 config.contentType.svg = 'image/svg+xml';
+config.contentType.pdf = 'application/pdf';
 config.port = 8888;
 config.root = './public/';
 
